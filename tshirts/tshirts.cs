b9fdbc27339d6499c73a077d7bcac53da7d97a15
Kavya-Basavaraj <132560975+Kavya-Basavaraj@users.noreply.github.com>
2024-10-26 08:47:31 +0530

    Update tshirts.cs

--- tshirts/tshirts.cs
+++ tshirts/tshirts.cs
@@ -13,8 +13,8 @@ namespace TshirtSpace {
             }
         }
         static void Main(string[] args) {
-            Debug.Assert(Size(39) == "S");
-            Debug.Assert(Size(40) == "M");
+            Debug.Assert(Size(38) == "S");
+            Debug.Assert(Size(42) == "M");
             Debug.Assert(Size(43) == "L");
             Console.WriteLine( "All is well (maybe!)");
         }
