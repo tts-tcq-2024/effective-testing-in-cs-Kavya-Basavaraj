7c864d13993d7c89e1d9a2f91280da70dca1763e
Kavya-Basavaraj <132560975+Kavya-Basavaraj@users.noreply.github.com>
2024-10-26 08:59:26 +0530

    Update misaligned.cs

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -9,7 +9,7 @@ namespace MisalignedSpace {
             int i = 0, j = 0;
             for(i = 0; i < 5; i++) {
                 for(j = 0; j < 5; j++) {
-                    Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i])
+                    Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);
                 }
             }
             return i * j;
