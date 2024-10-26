e29e6adeab9b4d853aaabe818768348f49c3ca68
Kavya-Basavaraj <132560975+Kavya-Basavaraj@users.noreply.github.com>
2024-10-26 08:55:28 +0530

    Update alerter.cs

--- alerter/alerter.cs
+++ alerter/alerter.cs
@@ -4,7 +4,7 @@ namespace AlerterSpace {
     class Alerter {
         static int alertFailureCount = 0;
         static int networkAlertStub(float celcius) {
-            Console.WriteLine("ALERT: Temperature is {0} celcius", celcius);
+            Console.WriteLine("ALERT: Temperature is {0} celcius", celcius)
             // Return 200 for ok
             // Return 500 for not-ok
             // stub always succeeds and returns 200
